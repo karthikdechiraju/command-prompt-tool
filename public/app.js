286dee586ac7bb317437c915a1f7aa5a3a46810e
Karthik Dechiraju <karthik@pyritetechnologies.com>
2018-02-25 00:05:29 +0530

    add synonym and antonym api

--- public/app.js
+++ public/app.js
@@ -43,6 +43,14 @@ cmd.controller('cmdCtrl',function($scope,$http,$sce){
 				$scope.definition(cmd_array[1].trim(),'new')
 				break;
 
+			case 'syn':
+					$scope.synonym(cmd_array[1].trim(),'new')
+					break;
+
+			case 'ant':
+				$scope.antonym(cmd_array[1].trim(),'new')
+				break;
+				
 			default:
 				if (cmd_array[1]) {
 					var li = '<div>Enter command: ./dict '+ cmd_array[0].trim() +' '+ cmd_array[1].trim() +'</div>';
@@ -77,6 +85,81 @@ cmd.controller('cmdCtrl',function($scope,$http,$sce){
 			.catch(function(err){
 				var li = '<div class="repeat-div-heading">Enter command: ./dict def '+ text+'</div>';
 				li = li + '<div>No definition found</div>'
+				$scope.prompt_array.push(li)
+				$scope.cmd_input = ""
+			})
+    }
+
+    $scope.synonym = function(text,command){
+    	$http.get('http://api.wordnik.com:80/v4/word.json/'+ text +'/relatedWords?&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5&relationshipTypes=synonym')
+			.then(function(response){
+				if (response.data.length) {
+					for (var i = 0; i < response.data.length; i++) {
+						if (response.data[i].relationshipType == "synonym") {
+							var syn_array = response.data[i].words;
+						}
+					}
+					if (syn_array) {
+						if (command && command == 'new') {
+							var li = '<div class="repeat-div-heading">Enter command: ./dict syn '+ text+'</div>';
+						}else{
+							var li = '<div class="repeat-div-heading">Synonyms of '+ text+'</div>';
+						}
+						for (var i = 0; i < syn_array.length; i++) {
+							li = li + '<div>'+ '<b> '+ parseInt(i + 1) +'. </b>'  + syn_array[i] +'</div>'
+						}
+					}else{
+						var li = '<div class="repeat-div-heading">Enter command: ./dict syn '+ text+'</div>';
+						li = li + '<div>No synonym found</div>'
+					}
+				}else{
+					var li = '<div class="repeat-div-heading">Enter command: ./dict syn '+ text+'</div>';
+					li = li + '<div>No synonym found</div>'
+				}
+				$scope.prompt_array.push(li)
+				$scope.cmd_input = ""
+			})
+			.catch(function(err){
+				console.log(err)
+				var li = '<div class="repeat-div-heading">Enter command: ./dict syn '+ text+'</div>';
+				li = li + '<div>No synonym foundzdfsdf</div>'
+				$scope.prompt_array.push(li)
+				$scope.cmd_input = ""
+			})
+    }
+
+    $scope.antonym = function(text,command){
+    	$http.get('http://api.wordnik.com:80/v4/word.json/'+ text +'/relatedWords?&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5&relationshipTypes=antonym')
+			.then(function(response){
+				if (response.data.length) {
+					for (var i = 0; i < response.data.length; i++) {
+						if (response.data[i].relationshipType == "antonym") {
+							var ant_array = response.data[i].words;
+						}
+					}
+					if (ant_array) {
+						if (command && command == 'new') {
+							var li = '<div class="repeat-div-heading">Enter command: ./dict ant '+ text+'</div>';
+						}else{
+							var li = '<div class="repeat-div-heading">Antonyms of '+ text+'</div>';
+						}
+						for (var i = 0; i < ant_array.length; i++) {
+							li = li + '<div>'+ '<b> '+ parseInt(i + 1) +'. </b>'  + ant_array[i] +'</div>'
+						}
+					}else{
+						var li = '<div class="repeat-div-heading">Enter command: ./dict ant '+ text+'</div>';
+						li = li + '<div>No antonym found</div>'
+					}
+				}else{
+					var li = '<div class="repeat-div-heading">Enter command: ./dict ant '+ text+'</div>';
+					li = li + '<div>No antonym found</div>'
+				}
+				$scope.prompt_array.push(li)
+				$scope.cmd_input = ""
+			})
+			.catch(function(err){
+				var li = '<div class="repeat-div-heading">Enter command: ./dict ant '+ text+'</div>';
+				li = li + '<div>No antonym found</div>'
 				$scope.prompt_array.push(li)
 				$scope.cmd_input = ""
 			})
--- server.js
+++ server.js
@@ -11,11 +11,6 @@ app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 app.use(express.static(__dirname + '/public'));
 app.use(function(req, res, next) {
-    // var allowedOrigins = ['http://localhost:8100'];
-    // var origin = req.headers.origin;
-    // if(allowedOrigins.indexOf(origin) > -1){
-    //   res.setHeader('Access-Control-Allow-Origin', origin);
-    // }
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials", "true");
     next();
