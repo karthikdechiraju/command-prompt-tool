eb3bb101f3d8cea33d8d9798e534b4483b7d7b6f
Karthik Dechiraju <karthik@pyritetechnologies.com>
2018-02-25 00:01:13 +0530

    added definition api

--- public/app.js
+++ public/app.js
@@ -36,4 +36,50 @@ cmd.controller('cmdCtrl',function($scope,$http,$sce){
         return $sce.trustAsHtml(string);
     };
 
+    $scope.cmd_submit = function(){
+		var cmd_array = $scope.cmd_input.split(' ');
+		switch(cmd_array[0].trim()){
+			case 'def':
+				$scope.definition(cmd_array[1].trim(),'new')
+				break;
+
+			default:
+				if (cmd_array[1]) {
+					var li = '<div>Enter command: ./dict '+ cmd_array[0].trim() +' '+ cmd_array[1].trim() +'</div>';
+				}else{
+					var li = '<div>Enter command: ./dict '+ cmd_array[0].trim() +'</div>';
+				}
+				li = li + '<div>'+ cmd_array[0].trim() +' is not defined</div>'
+				$scope.prompt_array.push(li)
+				$scope.cmd_input = ""
+		}
+	}
+
+	$scope.definition = function(text,command){
+    	$http.get('http://api.wordnik.com/v4/word.json/'+text+'/definitions?api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5')
+			.then(function(response){
+				if (response.data.length) {
+					if (command && command == 'new') {
+						var li = '<div class="repeat-div-heading">Enter command: ./dict def '+ text+'</div>';
+					}else{
+						var li = '<div class="repeat-div-heading">Definition of '+ text+'</div>';
+					}
+					for (var i = 0; i < response.data.length; i++) {
+						li = li + '<div>'+ '<b> '+ parseInt(i + 1) +'. </b>'  + response.data[i].text +'</div>'
+					}
+				}else{
+					var li = '<div class="repeat-div-heading">Enter command: ./dict def '+ text+'</div>';
+					li = li + '<div>No definition found</div>'
+				}
+				$scope.prompt_array.push(li)
+				$scope.cmd_input = ""
+			})
+			.catch(function(err){
+				var li = '<div class="repeat-div-heading">Enter command: ./dict def '+ text+'</div>';
+				li = li + '<div>No definition found</div>'
+				$scope.prompt_array.push(li)
+				$scope.cmd_input = ""
+			})
+    }
+
 })
